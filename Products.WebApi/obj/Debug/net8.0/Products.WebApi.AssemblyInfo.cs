e0bf967c7fdb4bd18d1805609ec1ba4892e82897
zappz88 <aramshaw88@gmail.com>
2024-11-10 08:34:25 -0600

    refactor arg names in controllers
    refactor arg names in controllers

--- Products.WebApi/obj/Debug/net8.0/Products.WebApi.AssemblyInfo.cs
+++ Products.WebApi/obj/Debug/net8.0/Products.WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Products.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db9f763f28766a3c83a61a5950dbbcc2d4cf293e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Products.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Products.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
